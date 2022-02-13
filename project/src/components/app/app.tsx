176aa88c56ee6b77bb04f8a2a55112552e4ddc9e
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-02-13 15:05:20 +0400

    Создал главную страницу

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type PromoProops = {
+  promoTitle: string;
+  promoGenre: string;
+  promoYear: number;
+}
+
+function App({promoTitle, promoGenre, promoYear}: PromoProops): JSX.Element {
+  return <MainPage promoTitle={promoTitle} promoGenre={promoGenre} promoYear={promoYear} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Promo = {
+  promoTitle: 'The Grand Budapest Hotel',
+  promoGenre: 'Drama',
+  promoYear: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoTitle={Promo.promoTitle}
+      promoGenre={Promo.promoGenre}
+      promoYear={Promo.promoYear}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
