1b4b8e772a2313fa1c90c3a0e2ce7195d1eb3f5b
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-02-13 16:02:48 +0400

    Создал страницы указанные в ТЗ

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -18,7 +18,7 @@ function MainPage ({promoTitle, promoGenre, promoYear}: MainPageProops): JSX.Ele
 
         <header className="page-header film-card__head">
           <div className="logo">
-            <a className="logo__link">
+            <a href="#" className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
@@ -32,7 +32,7 @@ function MainPage ({promoTitle, promoGenre, promoYear}: MainPageProops): JSX.Ele
               </div>
             </li>
             <li className="user-block__item">
-              <a className="user-block__link">Sign out</a>
+              <a href="#" className="user-block__link">Sign out</a>
             </li>
           </ul>
         </header>
