6b27b62e01c9f35067d6c78b887e4b5e24ad010a
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-02-16 14:39:06 +0400

    Описал все маршруты и 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
+import Error from '../error/error';
+import Login from '../login/login';
+import MyList from '../mylist/mylist';
+import Films from '../films/id/films';
+import AddReview from '../films/id/review/add-review';
+import Player from '../player/id/player';
 
 type PromoProops = {
   promoTitle: string;
@@ -7,7 +14,40 @@ type PromoProops = {
 }
 
 function App({promoTitle, promoGenre, promoYear}: PromoProops): JSX.Element {
-  return <MainPage promoTitle={promoTitle} promoGenre={promoGenre} promoYear={promoYear} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<MainPage promoTitle={promoTitle} promoGenre={promoGenre} promoYear={promoYear} />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/mylist'
+          element={<MyList />}
+        />
+        <Route
+          path='/films'
+          element={<Films />}
+        />
+        <Route
+          path='/add-review'
+          element={<AddReview />}
+        />
+        <Route
+          path='/player'
+          element={<Player />}
+        />
+        <Route
+          path='*'
+          element={<Error />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
